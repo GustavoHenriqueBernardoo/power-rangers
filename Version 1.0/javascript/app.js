98a7f520dcb55bf2b910b54ce7f3c745957988cc
Gustavo <gustavobernardo094@gmail.com>
2022-03-02 20:40:30 -0300

    Change the white color to green, update the app.js to show when active the information of each-ranger on the screen

--- Version 1.0/javascript/app.js
+++ Version 1.0/javascript/app.js
@@ -1,4 +1,6 @@
-const panels = document.querySelectorAll('.panel');
+// const panels = document.querySelectorAll('.panel');
+// const eachRanger = document.querySelector('.each-ranger');
+
 
 // Fetching the JSON file
 const getData = () => {
@@ -32,57 +34,58 @@ const getYear = () => {
 }
 
 // Creating a click eventListener to add class active and show content from JSON
-function handleClick(data){
-  panels.forEach((panel)=>{
-    panel.addEventListener('click', ((event) =>{
-      removeActive();
+function handleClick(data) {
+  const panels = document.querySelectorAll('.panel');
+  panels.forEach((panel) => {
+    panel.addEventListener('click', ((event)=> {
+      removeActiveClasses();
       panel.classList.add('active');
-      if(event.currentTarget.classList.contains('active')){
-        showJSON(data, event.currentTarget);
-      }
+      if(event.currentTarget.classList.contains("active")) {
+        showJson(data,event.currentTarget);
+      } 
     }))
   })
 }
 // Remove class active
-function removeActive(){
+function removeActiveClasses(){
+  const panels = document.querySelectorAll('.panel');
   panels.forEach((panel)=>{
     panel.classList.remove('active');
+    panel.style.pointerEvents = "auto";
   })
 }
 
 // Get the content from JSON to the HTML
-function showJSON(data, target){
-  console.log(target.id)
-  const h3 = document.querySelector('.panel h3')
-  data.power_rangers.forEach((ranger)=>{
-    target.innerHTML =`
-      <div class="each-ranger">
-        <p> ${ranger.name} </p>
-        <img src="${ranger.images}">
-        <p> ${ranger.power_level} </p>
-        <p> ${ranger.role} </p>
-        <p> ${ranger.abilities} </p>
-        <p> ${ranger.limitations} </p>
-        <div> ${ranger.abilities} </div>
-      </div>
-    `
-    const eachRanger = document.querySelector('.each-ranger');
-    // const redRanger = document.querySelector('pwrRed');
-    target.style.pointerEvents = 'none';
-    target.style.height = 'auto';
-    eachRanger.style.display = 'flex';
-    eachRanger.style.textAlign = 'center';
-    eachRanger.style.flexDirection = 'column';
-    eachRanger.style.padding = '10px';
-    eachRanger.style.color = 'black';
-    eachRanger.style.fontSize = '20px';
-    eachRanger.style.background = 'url(https://kanto.legiaodosherois.com.br/w750-h750-k1/wp-content/uploads/2015/06/49e5fc5ddeec05e9017939a675186e6c.jpg.jpeg) no-repeat center'
-    eachRanger.style.background.opacity = '15%';
-
+function showJson(data,target) {
+  data.power_rangers.forEach((ranger) => {
+    if(ranger.name.includes(target.id)) {
+      target.innerHTML = `
+        <div class="each-ranger"> 
+          <p> ${ranger.name} </p>
+          <img src="${ranger.images}">
+          <p> ${ranger.power_level} </p>
+          <p> ${ranger.role} </p>
+          <p> ${ranger.abilities} </p>
+          <p> ${ranger.limitations} </p>
+          <div> ${ranger.abilities} </div>
+        </div>
+      `
+      target.style.pointerEvents = "none";
+      target.style.height = "auto";
+      // const redRanger = document.querySelector('pwrRed');
+    // target.style.pointerEvents = 'none';
+    // target.style.height = 'auto';
+    // eachRanger.style.display = 'flex';
+    // eachRanger.style.textAlign = 'center';
+    // eachRanger.style.flexDirection = 'column';
+    // eachRanger.style.padding = '10px';
+    // eachRanger.style.color = 'black';
+    // eachRanger.style.fontSize = '20px';
+    // eachRanger.style.background = 'url(https://kanto.legiaodosherois.com.br/w750-h750-k1/wp-content/uploads/2015/06/49e5fc5ddeec05e9017939a675186e6c.jpg.jpeg) no-repeat center'
+    // eachRanger.style.background.opacity = '15%';
+    }
   })
-
 }
 
-handleClick();
 getData();
 getYear();
